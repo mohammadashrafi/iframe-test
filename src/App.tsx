204ce9c3e52fb1ee33c0e3eb7bea03caba181d63
mohammad ashrafi <mohammadashrafi7@gmail.com>
2024-06-02 12:43:44 +0330

    add redirect button

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,10 @@ const handelClick=()=>{
   return (
     <>
   
-     <iframe ref={myref} src="https://next-auth-session.vercel.app" width={"80%"} height={"400px"} title="Iframe Example" className='m-auto border border-6 border-red-950 '></iframe>
+     <iframe ref={myref} src="https://next-auth-session.vercel.app" sandbox='allow-same-origin allow-scripts allow-forms allow-popups-to-escape-sandbox' width={"80%"} height={"400px"} title="Iframe Example" className='m-auto border border-6 border-red-950 '></iframe>
      <button onClick={handelClick} className='bg-red-500 p-3 mt-4 rounded-md text-white'>click</button>
+     <br />
+     <a className='bg-green-500 block w-[100px] mx-auto p-3 mt-4 rounded-md text-white' href="https://next-auth-session.vercel.app/redirect">redirect</a>
     </>
   )
 }
