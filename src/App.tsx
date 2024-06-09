592bb0dc6bb09561c62d76b7b19a8ae176baff7e
mohammad ashrafi <mohammadashrafi7@gmail.com>
2024-06-09 15:48:53 +0330

    add allow to access cookie

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react'
 import './App.css'
-
+import CryptoJS from "crypto-js"
 function App() {
 const myref=useRef<HTMLIFrameElement>(null)
 const handelClick=()=>{
@@ -18,13 +18,45 @@ const handelClick=()=>{
   }
 }
 
+const obj={
+  name:"mohammad",lastname:"ashrafi",age:30
+}
+const secretkey="df5g4df564654d56fg54"
+
+const encryptFormData = (data:undefined, key:string) => {
+  const jsonString = JSON.stringify(data);
+  const encryptedString = CryptoJS.AES.encrypt(jsonString, key).toString();
+  return encryptedString;
+};
+
+
+const decryptFormData = (encryptedString:undefined, key:string) => {
+  const bytes = CryptoJS.AES.decrypt(encryptedString, key);
+  const decryptedString = bytes.toString(CryptoJS.enc.Utf8);
+  return JSON.parse(decryptedString);
+};
+
+
+
+
+const handelCript =async()=>{
+  const encryptForm= await encryptFormData(obj,secretkey)
+  const decript=await decryptFormData(encryptForm,secretkey)
+
+  console.log("encryptFormData",encryptForm)
+  console.log("decript",decript)
+}
+
+
+
   return (
     <>
   
-     <iframe ref={myref} src="https://next-auth-session.vercel.app" sandbox='allow-same-origin allow-scripts allow-forms allow-popups-to-escape-sandbox' width={"80%"} height={"400px"} title="Iframe Example" className='m-auto border border-6 border-red-950 '></iframe>
+     <iframe ref={myref} src="https://next-auth-session.vercel.app" sandbox='allow-same-origin' width={"80%"} height={"400px"} title="Iframe Example" className='m-auto border border-6 border-red-950 '></iframe>
      <button onClick={handelClick} className='bg-red-500 p-3 mt-4 rounded-md text-white'>click</button>
      <br />
      <a className='bg-green-500 block w-[100px] mx-auto p-3 mt-4 rounded-md text-white' href="https://next-auth-session.vercel.app/redirect">redirect</a>
+     <button onClick={handelCript}>crp</button>
     </>
   )
 }
